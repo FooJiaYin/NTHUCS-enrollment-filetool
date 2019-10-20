f2370c8a4667130a72810a39e1239b9ff96ef806
Foo Jia Yin <candy23198@gmail.com>
2019-10-20 11:40:24 +0800

    First Commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# 審查資料分類工具操作說明
+
+## 檔案
+1. filename.xlsx
+2. file_operation.bat
+
+## 使用步驟
+1. 將excel的新舊名稱貼到`filename.xlsx`的Original:New
+![](https://i.imgur.com/pHqwHdW.png) ![](https://i.imgur.com/5kE5vbJ.png)   ![](https://i.imgur.com/1manAcu.png)
+
+2. 複製Command中的資料（D:F 從ren開始）
+**行數不夠可以增加
+![](https://i.imgur.com/MeyLy1h.png)
+
+
+3. 按右鍵**編輯** `file_operation.bat`，在`:: Rename ::`底下貼上資料
+![](https://i.imgur.com/XUPxYpR.png) ![](https://i.imgur.com/ko19AdY.png) ![](https://i.imgur.com/ihApnbc.png)
+
+儲存並關閉。
+
+4. 雙點`file_operation.bat`，程式就會執行。
---
+++ file_operation.bat
@@ -0,0 +1,43 @@
+:: Rename ::
+ren	1.pdf	1-A-B-C.pdf
+ren	2.pdf	2-A-B.pdf
+ren	3.pdf	3-C.pdf
+
+:: Categorize ::
+:: Do not change the commands after this line!! ::
+mkdir A
+mkdir B
+mkdir C
+mkdir D
+mkdir E
+mkdir F
+mkdir G
+mkdir H
+mkdir I
+mkdir J
+mkdir K
+mkdir L
+mkdir M
+mkdir N
+mkdir O
+mkdir P
+mkdir Q
+mkdir R
+Robocopy .\ A *A*.pdf
+Robocopy .\ B *B*.pdf
+Robocopy .\ C *C*.pdf
+Robocopy .\ D *D*.pdf
+Robocopy .\ E *E*.pdf
+Robocopy .\ F *F*.pdf
+Robocopy .\ G *G*.pdf
+Robocopy .\ H *H*.pdf
+Robocopy .\ I *I*.pdf
+Robocopy .\ J *J*.pdf
+Robocopy .\ K *K*.pdf
+Robocopy .\ L *L*.pdf
+Robocopy .\ M *M*.pdf
+Robocopy .\ N *N*.pdf
+Robocopy .\ O *O*.pdf
+Robocopy .\ P *P*.pdf
+Robocopy .\ Q *Q*.pdf
+Robocopy .\ R *R*.pdf
